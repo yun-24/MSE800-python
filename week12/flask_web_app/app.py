7cc8755827612c59ae666c825165f7cf1d5893c5
yun <188809071+yun-24@users.noreply.github.com>
2025-02-24 09:50:08 +0800

    exercise: add another page

--- week12/flask_web_app/app.py
+++ week12/flask_web_app/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/second')
+def second():
+    return render_template('second.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
