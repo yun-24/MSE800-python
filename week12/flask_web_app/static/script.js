b5aeaa334781813e1372e83da0b35cffb1c72d32
yun <188809071+yun-24@users.noreply.github.com>
2025-02-24 09:41:46 +0800

    exercise: flask and html

---
+++ week12/flask_web_app/static/script.js
@@ -0,0 +1,7 @@
+document.getElementById('alertBtn').addEventListener('click', function() {
+    alert('Hello from JavaScript!');
+});
+
+document.getElementById('helloBtn').addEventListener('click', function() {
+    confirm("world")
+});
