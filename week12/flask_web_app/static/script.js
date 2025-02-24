39d73131a20984d19b4d85f1e13971b6fe1c81ef
yun <188809071+yun-24@users.noreply.github.com>
2025-02-24 10:37:47 +0800

    exercise: go to home when click button

--- week12/flask_web_app/static/script.js
+++ week12/flask_web_app/static/script.js
@@ -1,7 +1,12 @@
-document.getElementById('alertBtn').addEventListener('click', function() {
+document.getElementById('alertBtn')?.addEventListener('click', function() {
     alert('Hello from JavaScript!');
 });
 
-document.getElementById('helloBtn').addEventListener('click', function() {
+document.getElementById('helloBtn')?.addEventListener('click', function() {
     confirm("world")
 });
+
+document.getElementById('homeBtn')?.addEventListener('click', function() {
+    window.location.href = "/"
+});
+
